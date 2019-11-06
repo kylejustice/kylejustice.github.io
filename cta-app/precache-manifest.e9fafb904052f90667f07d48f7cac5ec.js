49b19e90f02e00786044d64e43719cde6cfa6106
Kyle Justice <kyle_justice@yahoo.com>
2019-11-06 12:51:07 -0600

    Uploading initial CTA Dashboard App under /cta-app

---
+++ cta-app/precache-manifest.e9fafb904052f90667f07d48f7cac5ec.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "f7a2466c22b3aec3a688",
+    "url": "/bundle.d0811.css"
+  },
+  {
+    "revision": "f7a2466c22b3aec3a688",
+    "url": "/bundle.f7a24.js"
+  },
+  {
+    "revision": "9d8ca469f63bfc473ec482626a2834e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e49433b3f223e35c6dd3",
+    "url": "/polyfills.e4943.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "6492322e01b59faaebf253383fb78b98",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "beec18519751ae79bfb6c12cb9a3246f",
+    "url": "/sw.js"
+  }
+]);
