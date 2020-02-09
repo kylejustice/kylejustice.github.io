b80f400b9c68fc7267e1cbcbe7aac48c4339e0fb
Kyle Justice <kyle_justice@yahoo.com>
2020-02-09 13:35:35 -0600

    Updating favicon and page titles for CTA App.

---
+++ cta-app/b681487927bbb2be0708c8fcff162207839a4235/precache-manifest.23394b89a370984eef35f654335e2744.esm.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "0a9628f0c3a302c75f6a0a5a8b623419",
+    "url": "/bundle.0f3d4.esm.js"
+  },
+  {
+    "revision": "2d2575bb801fc0736172",
+    "url": "/bundle.f6664.css"
+  },
+  {
+    "revision": "f6e1ea91388fc90362e652c997eb32c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cd7afbd58bcda2e21b03b49324dd6db",
+    "url": "/polyfills.1e329.esm.js"
+  }
+]);
