c10ee8e38bf8d52ad8045f87a2c59829a0b45010
Kyle Justice <kyle_justice@yahoo.com>
2020-03-08 22:14:02 -0500

    Updating CTA App childs to form in columns on desktop and up, lowering font size of large, and adding theme-color for Chrome tabs.

---
+++ cta-app/b681487927bbb2be0708c8fcff162207839a4235/precache-manifest.9a76448353a128e5b26148947c29fa79.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "0923a971eb008d4f1f0c",
+    "url": "/bundle.0923a.js"
+  },
+  {
+    "revision": "0923a971eb008d4f1f0c",
+    "url": "/bundle.87227.css"
+  },
+  {
+    "revision": "f2d6372a4ba05bec47b9a3fbbcb336a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e49433b3f223e35c6dd3",
+    "url": "/polyfills.e4943.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "c88145066c11636cada7a0cf3eb5efda",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "beec18519751ae79bfb6c12cb9a3246f",
+    "url": "/sw.js"
+  }
+]);
---
+++ cta-app/b681487927bbb2be0708c8fcff162207839a4235/precache-manifest.9ad6e0a8dff52563c4ea6a32b1a58c8e.esm.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "b7da1ab8e43bb8d4eda50953049d09af",
+    "url": "/bundle.083d7.esm.js"
+  },
+  {
+    "revision": "0923a971eb008d4f1f0c",
+    "url": "/bundle.87227.css"
+  },
+  {
+    "revision": "f2d6372a4ba05bec47b9a3fbbcb336a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cd7afbd58bcda2e21b03b49324dd6db",
+    "url": "/polyfills.1e329.esm.js"
+  }
+]);
