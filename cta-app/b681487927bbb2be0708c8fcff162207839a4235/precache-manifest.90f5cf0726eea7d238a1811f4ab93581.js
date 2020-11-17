3fb380835c24fb450aad675f94c6876c79b48d21
kylejustice <kyle_justice@yahoo.com>
2020-11-16 18:31:11 -0600

    Updating the Preact CTA App with the corrected subfolder path.

---
+++ cta-app/b681487927bbb2be0708c8fcff162207839a4235/precache-manifest.90f5cf0726eea7d238a1811f4ab93581.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "57bb47c92f6ae065a4ef",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/bundle.57bb4.js"
+  },
+  {
+    "revision": "57bb47c92f6ae065a4ef",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/bundle.bd587.css"
+  },
+  {
+    "revision": "2102653d7211bb86a04d80740bc07ac3",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/index.html"
+  },
+  {
+    "revision": "b9b4364cc30994c4bfed",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/polyfills.b9b43.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/sw-debug.js"
+  },
+  {
+    "revision": "9b39b198dd8a41b9cf3e8b4730db1d50",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/sw-esm.js"
+  },
+  {
+    "revision": "21b213ae158c1868a0e59bb2504daf2f",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/sw.js"
+  }
+]);
