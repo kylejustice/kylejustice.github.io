11e2a1af53db7d8f4b4153c054147733d721e118
kylejustice <kyle_justice@yahoo.com>
2021-06-17 16:58:15 -0500

    Updating the Preact CTA App to allow for Refreshing the results via a control button.

---
+++ cta-app/b681487927bbb2be0708c8fcff162207839a4235/precache-manifest.767d59807d0470e9496037ba28146b7c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "04d6796490862b567cca",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235bundle.04d67.js"
+  },
+  {
+    "revision": "04d6796490862b567cca",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235bundle.d3418.css"
+  },
+  {
+    "revision": "ddda1fe59d2a6bb70855f7ccafb402fd",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235index.html"
+  },
+  {
+    "revision": "a69805ab02dfe6fc89e7",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235polyfills.a6980.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235sw-debug.js"
+  },
+  {
+    "revision": "ba78ad1d8743af6c3c5c64b6ff634f25",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235sw-esm.js"
+  },
+  {
+    "revision": "4254757ed050c59b4c72f46e74e31fd1",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235sw.js"
+  }
+]);
