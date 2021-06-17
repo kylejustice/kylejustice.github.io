e01bc1abc9a308a8c3e4b8516a04802f6523c66d
kylejustice <kyle_justice@yahoo.com>
2021-06-17 17:04:59 -0500

    Updating the Preact CTA App with the corrected subfolder path.

---
+++ cta-app/b681487927bbb2be0708c8fcff162207839a4235/precache-manifest.09adffc0fa9c7b53450bbacf27edf41e.esm.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "b73638f3074110d6fbb36adc3946b5de",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/bundle.36a11.esm.js"
+  },
+  {
+    "revision": "b74a341e3cde948d659b",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/bundle.8e1fc.css"
+  },
+  {
+    "revision": "d6cbc405609c1e4c7249ed6e3247a0fd",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/index.html"
+  },
+  {
+    "revision": "5ccfde0bdd6b849bfc994e167da6e74b",
+    "url": "/cta-app/b681487927bbb2be0708c8fcff162207839a4235/polyfills.978c4.esm.js"
+  }
+]);
